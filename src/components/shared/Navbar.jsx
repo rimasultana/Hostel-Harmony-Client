9862b4a5414cde95e7b3da9b4f8d56a1994346a0
rimasultana <rimasultana20225@gmail.com>
2025-02-19 19:26:57 +0600

    navbar width fixd

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
           : "bg-white border-b border-gray-200"
       )}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-11/12 mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Left: Logo and Desktop Links */}
           <div className="flex items-center space-x-4">
